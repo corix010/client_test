c63dc8897a9c07dfa7b177a7beb23d7f16a88020
corix <rico.c.marcelo@gmail.com>
2012-04-20 23:30:46 -0500

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+require 'net/http'
+class HomeController < ApplicationController
+  def index
+    url = URI.parse('http://localhost:3002/vehicles.json?id=1')
+    # req = Net::HTTP::Get.new(url.path)
+    req = Net::HTTP::Get.new("http://localhost:3002/vehicles.json?id=1")
+    res = Net::HTTP.start(url.host, url.port) {|http|
+    http.request(req)
+    }
+    parsed_json = ActiveSupport::JSON.decode(res.body)
+    raise parsed_json.inspect
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClientTest::Application.config.secret_token = '2bd64121c410f9abc7036fda58260a653583dd0efbc5efcf5849d613f61e6a03b7000b3505a5567f12dd22b566bca8fe2dd15ca2e30b85277e362d8cefc556db'
